226241999330d62f92f512b50c0ab0112dfdf66c
Jack_specs <test@email>
2025-05-25 19:23:04 -0400

    modified menu_response .upper

--- Johan.py
+++ Johan.py
@@ -15,14 +15,14 @@ print(f"""Thank you {user_name} for coming to RC Hookah Lounge!
 We got 3 Hookah flavors and 3 drinks
 A hookah cost $20 (combine any flavor free of charge) and a drink cost $3
 Would you like to know what they are?""")
-menu_response = input("y for Yes and n for No (y/n)")
-if menu_response == "y":
+menu_response = input("Y for Yes and N for No (Y/N)"). upper()
+if menu_response == "Y":
     print("""Great! For flavors we have Mint, Blueberry, Cherry and Cappucino
 And for drinks we have Soda, Hot Tea and Coffee""")
-elif menu_response == "n":
+elif menu_response == "N":
     print(random.choice(no_responses))
     
 else: 
-    print("Sorry I didn't quite catch that. Please type 'y' for Yes or 'n' for No.")
+    print("Sorry I didn't quite catch that. Please type 'Y' for Yes or 'N' for No.")
 #Flavors =[" Mint", "Cherry"," Capaocinno"]
 #Drinks = ["Soda", ,Tea " Coffee"]
